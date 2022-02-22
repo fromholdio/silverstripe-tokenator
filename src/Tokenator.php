1f805d3d65f5019a4810209a90959c9d425322e2
Luke Fromhold <luke@fromhold.io>
2022-02-22 14:44:39 +1100

    Initial commit

---
+++ src/Tokenator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace VisualMetrics\HSU\Core\Extensions;
+
+use SilverStripe\ORM\DataExtension;
+
+class Tokenator extends DataExtension
+{
+    private static $tokenator_field;
+    private static $tokenator_char_length;
+
+    public static function generate_tokenator(?int $charLength = null)
+    {
+        if (empty($charLength)) {
+            $charLength = 12;
+        }
+        $token = '';
+        $codeAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+        $codeAlphabet .= '0123456789';
+        $max = strlen($codeAlphabet);
+        for ($i = 0; $i < $charLength; $i++) {
+            $token .= $codeAlphabet[random_int(0, $max - 1)];
+        }
+        return $token;
+    }
+
+    public function refreshTokenator()
+    {
+        $tokenatorField = $this->getOwner()->config()->get('tokenator_field');
+        if (!empty($tokenatorField))
+        {
+            $charLength = (int) $this->getOwner()->config()->get('tokenator_char_length');
+            if ($charLength < 1) {
+                $charLength = null;
+            }
+            $token = self::generate_tokenator($charLength);
+
+            if ($this->getOwner()->hasMethod('getTokenatorScope')) {
+                $scope = $this->getOwner()->getTokenatorScope();
+                $scope->filter($tokenatorField, $token);
+            }
+            else {
+                $class = get_class($this->getOwner());
+                $scope = $class::get()->filter($tokenatorField, $token);
+                if ($this->getOwner()->isInDB()) {
+                    $scope = $scope->exclude('ID', $this->getOwner()->ID);
+                }
+            }
+
+            if ($scope->count() > 0) {
+                return $this->getOwner()->refreshTokenator();
+            }
+
+            $this->getOwner()->{$tokenatorField} = $token;
+        }
+        return $this->getOwner();
+    }
+
+    public function onBeforeWrite()
+    {
+        $tokenatorField = $this->getOwner()->config()->get('tokenator_field');
+        if (!empty($tokenatorField) && empty($this->getOwner()->{$tokenatorField})) {
+            $this->getOwner()->refreshTokenator();
+        }
+    }
+}
